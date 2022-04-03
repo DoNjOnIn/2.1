5ed7f8317314dbcaca0f88511f6258db0f5a4d3b
DoNjOnIn <kaijaroslav@gmail.com>
2022-04-03 14:46:22 +0300

    Add project files.

---
+++ 2.1/2.1.cpp
@@ -0,0 +1,16 @@
+//2.1.cpp
+#include <iostream>
+#include "Floatrange.h"
+int main()
+{
+    Floatrange a(20, 1);
+    cout << a << endl;
+    cout << ++a << endl;
+    cout << a << endl;
+    cout << a++ << endl;
+    cout << a << endl;
+    Floatrange x;
+    cout << "Input complex number ->" << endl;
+    cin >> x;
+    x.RangeCheck();
+}
